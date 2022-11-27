c81deb7c04846ceca6832caf1188b5857872930a
クンテインゾー <87855489+Htet04@users.noreply.github.com>
2022-11-27 18:50:00 +0630

    testing code

--- app/src/main/java/com/codewall/keeplinks/MainActivity.java
+++ app/src/main/java/com/codewall/keeplinks/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.codewall.keeplinks.databinding.ActivityMainBinding;
+import com.codewall.keeplinks.ui.fragment.BrowserFragment;
 import com.codewall.keeplinks.ui.fragment.CategoryFragment;
 import com.codewall.keeplinks.ui.fragment.HomeFragment;
 
@@ -46,12 +47,10 @@ public class MainActivity extends AppCompatActivity {
                     showFragment(new CategoryFragment());
                     break;
                 }
-                /*
-                tempo code
-                case R.id.menu_bnv_favorite: {
-                    showFragment(new FavoriteFragment());
+                case R.id.browser:{
+                    showFragment(new BrowserFragment());
                     break;
-                }*/
+                }
             }
             return true;
         });
---
+++ app/src/main/java/com/codewall/keeplinks/ui/fragment/BrowserFragment.java
@@ -0,0 +1,49 @@
+package com.codewall.keeplinks.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebViewClient;
+
+import com.codewall.keeplinks.R;
+import com.codewall.keeplinks.databinding.FragmentBrowserBinding;
+
+public class BrowserFragment extends Fragment {
+
+    FragmentBrowserBinding binding;
+
+    public BrowserFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentBrowserBinding.inflate(inflater,container,false);
+        binding.webView.loadUrl("https://speed.hetzner.de/");
+        binding.webView.setWebViewClient(new WebViewClient());
+        binding.webView.setWebChromeClient(new WebChromeClient());
+        binding.webView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> {
+            // Nothing just testing code
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setTitle("Title");
+            StringBuilder str = new StringBuilder();
+            str.append("url: ").append(url);
+            str.append("\nuserAgent: ").append(userAgent);
+            str.append("\ncontentDisposition: ").append(contentDisposition);
+            str.append("\nmimeType: ").append(mimetype);
+            str.append("\ncontentLength:").append(contentLength / 1024 / 1024);
+            builder.setMessage(str.toString());
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
+        return binding.getRoot();
+    }
+}
