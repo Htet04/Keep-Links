32728feec8a681faa3adc0de3511384256f844c6
クンテインゾー <87855489+Htet04@users.noreply.github.com>
2022-12-20 13:57:36 +0630

    UI or UX or something else

--- app/src/main/java/com/codewall/keeplinks/ui/dialog/AddCategoryDialog.java
+++ app/src/main/java/com/codewall/keeplinks/ui/dialog/AddCategoryDialog.java
@@ -1,5 +1,6 @@
 package com.codewall.keeplinks.ui.dialog;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.widget.Toast;
@@ -7,18 +8,20 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 
+import com.codewall.keeplinks.R;
 import com.codewall.keeplinks.databinding.DialogAddCategoryBinding;
 
 public class AddCategoryDialog extends AlertDialog {
     DialogAddCategoryBinding binding;
     public AddCategoryDialog(@NonNull Context context) {
         super(context);
-        this.setTitle("Add Category");
         binding = DialogAddCategoryBinding.inflate(LayoutInflater.from(context));
         this.setView(binding.getRoot());
+        this.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         binding.btnAddDialog.setOnClickListener(v -> {
             Toast.makeText(context, binding.textCategory.getText().toString(), Toast.LENGTH_SHORT).show();
             this.dismiss();
         });
+        Dialog dialog = new Dialog(context);
     }
 }
